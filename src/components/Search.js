b0ae94ac174d41248fdb8c2ae0b82661fc40fe8b
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-05 05:57:42 +0300

    GrSearch icon eklendi, icon class'ına ait stiller düzenlendi

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,12 @@
 import {userState} from 'react'
+import {GrSearch} from 'react-icons/gr'
 
 function Search(props) {
     return(
         <div className="search">
+            <div className="icon">
+                <GrSearch/>
+            </div>
             <input type="text" placeholder="Search Brands"/>
         </div>
     )
