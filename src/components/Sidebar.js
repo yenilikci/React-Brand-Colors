1ebf4f131185362c303efffa2ee1ed93d92cba02
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-05 04:27:29 +0300

    react-modal paketi dahil edildi, toggleModal fonk yazıldı

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,13 @@
 import {useState} from 'react'
+import Modal from 'react-modal'
 
 function Sidebar(props){
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const toggleModal = () => {
+        setModalIsOpen(!modalIsOpen);
+    }
+
     return(
         <>
            <aside className="sidebar">
@@ -13,11 +20,25 @@ function Sidebar(props){
                 <nav className="menu">
                     <ul>
                         <li>
-                            <a>About BrandColors</a>
+                            <a onClick={toggleModal}>About BrandColors</a>
                         </li>
                     </ul>
                 </nav>
            </aside>
+           <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={toggleModal}
+            >
+            <button onClick={toggleModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+                <input />
+                <button>tab navigation</button>
+                <button>stays</button>
+                <button>inside</button>
+                <button>the modal</button>
+            </form>
+           </Modal>
         </>
     )
 }
