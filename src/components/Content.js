c8b64e5f95cf60abe4c4e413d46d06ce2b690ea2
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-05 06:06:20 +0300

    brands.json'dan verilerin getirilmesi ve dizi içerisine yerleştirilmesi

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,24 @@
 import {useState} from 'react'
 import Search from './Search'
+import BrandsData from '../brands.json'
 
 function Content(props) {
+    
+    const brandsArray = []
+    Object.keys(BrandsData).map(key => {
+        brandsArray.push(BrandsData[key])
+    })
+
+    const [brands, setBrands] = useState(brandsArray)
+
     return(
         <main className="content">
             <header className="header">
                 <Search/>
             </header>
+            <section className="brands"> 
+                {brands.map(brand => ())}
+            </section>
         </main>
     )
 }
