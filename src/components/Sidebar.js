5ded22ebff7665dd3d1f868eeb552871feb1dbe0
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-05 04:16:03 +0300

    sidebar ve içerisindeki elemanların stilleri tanımlandı

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,19 +3,21 @@ import {useState} from 'react'
 function Sidebar(props){
     return(
         <>
-            <div className="logo">
-                <a>Brand<b>Colors</b></a>
-            </div> 
-            <div className="description">
-                The biggest collection of official brand color codes around. Curated by @brandcolors and friends.
-            </div> 
-            <nav className="menu">
-                <ul>
-                    <li>
-                        <a>About BrandColors</a>
-                    </li>
-                </ul>
-            </nav>
+           <aside className="sidebar">
+                <div className="logo">
+                    <a>Brand<b>Colors</b></a>
+                </div> 
+                <div className="description">
+                    The biggest collection of official brand color codes around. Curated by @brandcolors and friends.
+                </div> 
+                <nav className="menu">
+                    <ul>
+                        <li>
+                            <a>About BrandColors</a>
+                        </li>
+                    </ul>
+                </nav>
+           </aside>
         </>
     )
 }
