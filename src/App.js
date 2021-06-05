3b3a4d57b3aa733ffcdcd780b5d1500f8db48b9b
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-05 05:34:59 +0300

    Content component oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Sidebar from './components/Sidebar'
+import Content from './components/Content'
 
 function App() {
   return (
+    <>
     <Sidebar/>
+    <Content/>
+    </>
   );
 }
 
---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+
+function Content(props) {
+    return(
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Content
