7f12528128e6904ed273e19d3da4bf80b96fecee
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-05 04:42:00 +0300

    modal için className ve overlayClassName eklenip stilleri düzenlendi

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -28,16 +28,19 @@ function Sidebar(props){
            <Modal
             isOpen={modalIsOpen}
             onRequestClose={toggleModal}
+            className="about-modal"
+            overlayClassName="about-modal-overlay"
             >
             <button onClick={toggleModal}>close</button>
-            <div>I am a modal</div>
-            <form>
-                <input />
-                <button>tab navigation</button>
-                <button>stays</button>
-                <button>inside</button>
-                <button>the modal</button>
-            </form>
+            <h3>
+                About BrandColors
+            </h3>
+            <p>
+                BrandColors was created by DesignBombs. The goal was to create a helpful reference for the brand color codes that are needed most often.
+            </p>
+            <p>
+                It's been featured by Smashing Magazine, CSS-Tricks, Web Design Depot, Tuts+, and over 2 million pageviews. There are now over 600 brands with 1600 colors and the collection is always growing.
+            </p>
            </Modal>
         </>
     )
