7b0fd4270cd555c912ce54bf6d367f3477dc2736
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-05 03:59:02 +0300

    Sidebar componente description ve menu kısmı eklendi

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,16 @@ function Sidebar(props){
             <div className="logo">
                 <a>Brand<b>Colors</b></a>
             </div> 
+            <div className="description">
+                The biggest collection of official brand color codes around. Curated by @brandcolors and friends.
+            </div> 
+            <nav className="menu">
+                <ul>
+                    <li>
+                        <a>About BrandColors</a>
+                    </li>
+                </ul>
+            </nav>
         </>
     )
 }
