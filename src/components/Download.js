e82bf979573ca5b29ee223cfa44fec9c890a959f
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-06-08 18:27:36 +0300

    Blob objesi , indirme özelliğinin eklenmesi

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,18 +1,44 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import MainContext from "../MainContext";
 import { GrLink, GrDownload, GrClose } from "react-icons/gr";
 
 function Download() {
-  const { selectedBrands, setSelectedBrands } = useContext(MainContext);
+
+  const { selectedBrands, brands, setSelectedBrands } = useContext(MainContext); 
+  const [downloadUrl, setDownloadUrl] = useState()
+
+  useEffect(() => {
+    if(selectedBrands.length > 0){
+        let output = ''
+        selectedBrands.map(slug => {
+            let brand = brands.find(brand => brand.slug === slug)
+            brand.colors.map((color,key) => {
+                output += `--${slug}-${key}: #${color}\n`
+            })
+        })
+        const blob = new Blob([output])
+        const url = URL.createObjectURL(blob)
+        setDownloadUrl(url)
+        return () => {
+            URL.revokeObjectURL(url)
+            setDownloadUrl('')
+        }
+    }
+  },[selectedBrands])
+ 
+
+  const getLink = () => {
+      prompt('Heres\'s the URL to share',`http://localhost:3000/collection/${selectedBrands.join(',')}`)
+  }
 
   return (
     <div className="download">
       
       <div className="actions">
-        <a href="">
+        <a download="test.css" href={downloadUrl}>
             <GrDownload/>
         </a>
-        <button>
+        <button onClick={getLink}>
             <GrLink/>
         </button>
       </div>
