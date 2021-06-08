aa98e289a7d7068c7be802fffa9ae2b76d26252c
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-06-08 17:33:20 +0300

    GrDownload ve GrLink iconları ile birlikte bağlantı ve indirme elementleri eklendi

--- src/components/Download.js
+++ src/components/Download.js
@@ -3,15 +3,25 @@ import MainContext from "../MainContext";
 import { GrLink, GrDownload, GrClose } from "react-icons/gr";
 
 function Download() {
-  
-    const { selectedBrands,setSelectedBrands } = useContext(MainContext);
+  const { selectedBrands, setSelectedBrands } = useContext(MainContext);
 
   return (
     <div className="download">
+      
+      <div className="actions">
+        <a href="">
+            <GrDownload/>
+        </a>
+        <button>
+            <GrLink/>
+        </button>
+      </div>
+
       <div className="selected" onClick={() => setSelectedBrands([])}>
         <GrClose />
         {selectedBrands.length} brands collected
       </div>
+    
     </div>
   );
 }
