e64535fb5a2f23218381d6690bd92b8ce69910b4
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-06-08 14:43:45 +0300

    Seçilen renk bildirim arkaplan renk ile aynı olacak şekilde düzenlendi, yazı rengi için kontrast helper'ı da kullanıldı

--- src/App.js
+++ src/App.js
@@ -1,48 +1,47 @@
-import './App.css';
-import MainContext from './MainContext'
-import Sidebar from './components/Sidebar'
-import Content from './components/Content'
-import {useState,useEffect} from 'react'
-import BrandsData from './brands.json'
-import Copied from './components/Copied';
+import "./App.css";
+import MainContext from "./MainContext";
+import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
+import { useState, useEffect } from "react";
+import BrandsData from "./brands.json";
+import Copied from "./components/Copied";
 
 function App() {
+  const brandsArray = [];
+  Object.keys(BrandsData).map((key) => {
+    brandsArray.push(BrandsData[key]);
+  });
 
-  const brandsArray = []
-  Object.keys(BrandsData).map(key => {
-      brandsArray.push(BrandsData[key])
-  })
-
-  const [brands, setBrands] = useState(brandsArray)
-  const [selectedBrands, setSelectedBrands] = useState([])
-  const [copied, setCopied] = useState(false)
-
-  useEffect(() => {
-    console.log(selectedBrands)
-  },[selectedBrands])
-
+  const [brands, setBrands] = useState(brandsArray);
+  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
-    if(copied){
-      setTimeout(() => {
-        setCopied(false);
-      },1000)
-    }
-  }, [copied])
+    console.log(selectedBrands);
+  }, [selectedBrands]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [copied]);
 
   const data = {
     brands,
     selectedBrands,
     setSelectedBrands,
-    setCopied
-  }
+    setCopied,
+  };
 
   return (
     <>
       <MainContext.Provider value={data}>
         {copied && <Copied color={copied} />}
-        <Sidebar/>
-        <Content/>
+        <Sidebar />
+        <Content />
       </MainContext.Provider>
     </>
   );
--- src/components/Copied.js
+++ src/components/Copied.js
@@ -1,8 +1,9 @@
 import {useState} from 'react'
+import {getContrastYIQ} from '../helpers'
 
 function Copied({color}) {
     return (
-        <div className="copied">
+        <div className="copied" style={{'--bgColor':`#${color}`,'--textColor':`${getContrastYIQ(color)}`}}>
             Copied  #{color} to Clipboard
         </div>
     )
